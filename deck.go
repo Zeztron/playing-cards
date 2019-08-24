7da761a867aa3b52272be60fb450cc632d3a6746
Zeztron <pateltharsh@gmail.com>
2019-08-24 13:13:49 -0400

    created newDeck function

--- deck.go
+++ deck.go
@@ -3,8 +3,27 @@ package main
 import "fmt"
 
 // Create a new type of deck
-// Which is a type of strings
+// Which is an array of strings
 type deck []string
+
+// Creates and returns a list of playing cards
+// Returns a value of type deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// Replaced iterators with underscores if iterators are not used.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			// Append it to cards
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 
 // The d is a reference to cards
 func (d deck) print() {
--- main.go
+++ main.go
@@ -2,13 +2,7 @@ package main
 
 func main() {
 	// slice of string (array)
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-// Notifying that the function will return a type of string
-func newCard() string {
-	return "Five of Diamonds"
 }
